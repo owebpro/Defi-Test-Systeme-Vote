ad3e83709c5925c4eb70b2f93ebdd6591d0d2684
Your Name <you@example.com>
2025-02-25 00:23:10 +0100

    Final test added + description modifyed

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-Voting Smart Contract can be tested with this command:
+Simple voting system as a Smart Contract can be tested with this command:
 
 ```shell
 yarn hardhat test test/Voting.ts
 ```
 
+Ownership and partial errors are tested.
+Events returned checks.
+Final result verified.
+
 Then should return :
 
   Voting
     Deployment
-      ✔ Should deploy the smart contract
+      ✔ Should deploy the smart contract (516ms)
       ✔ Should set the right owner
     After deployment
       ✔ winningProposalID should be equal to 0
@@ -19,20 +23,21 @@ Then should return :
       ✔ Only owner should start voting session
       ✔ Only owner should end voting session
       ✔ Only owner should tally votes
-      ✔ Should revert with voter Voters registration is not open yet
-      ✔ Should revert with voter Already registered
+      ✔ Should find registered voter
       ✔ Should emit event VoterRegistered
+      ✔ Should revert with voter Already registered
+      ✔ Should revert with voter Voters registration is not open yet
       ✔ Should revert with Proposals are not allowed yet
       ✔ Should revert with 'Vous ne pouvez pas ne rien proposer'
-      ✔ Should find registered voter
       ✔ Should emit event WorkflowStatusChange after startProposalsRegistering
+      ✔ Should retrieve added proposal
+      ✔ Should emit event ProposalRegistered
       ✔ Should emit event WorkflowStatusChange after endProposalsRegistering
       ✔ Should emit event WorkflowStatusChange after startVotingSession
       ✔ Should emit event WorkflowStatusChange after endVotingSession
-      ✔ Should emit event WorkflowStatusChange after tallyVotes
-      ✔ Should retrieve added proposal
-      ✔ Should emit event ProposalRegistered
       ✔ Should retrieve added vote to proposal and emit event Voted
+      ✔ winningProposalID should be equal to 2 after tallyVotes
+      ✔ Should emit event WorkflowStatusChange after tallyVotes
 
 
-  24 passing
+  25 passing
--- test/Voting.ts
+++ test/Voting.ts
@@ -16,6 +16,8 @@ describe("Voting", () => {
 	}
 
 	describe("Deployment", () => {
+
+		// Deployment checks
 		
 		it("Should deploy the smart contract", async () => {
 			const { voting, owner, voter1, voter2, voter3 } = await loadFixture(deployContract);
@@ -42,16 +44,17 @@ describe("Voting", () => {
 			voting = await Voting.deploy();
 		});
 		
+		// Initialization checks
 		it("winningProposalID should be equal to 0", async () => {
 			const initialWiningProposalIdValue = await voting.winningProposalID();
 			expect(initialWiningProposalIdValue).to.equal(0);
 		});
 		
+		
+		// Restrictions checks
 		it("Only voters should get voters", async () => {
 		    await expect(voting.getVoter(voter1)).to.be.revertedWith("You're not a voter");
 		});
-
-
 		
 		it("Only owner should add voters", async () => {
 		    await expect(voting.connect(voter2).addVoter(voter2.address))
@@ -89,18 +92,12 @@ describe("Voting", () => {
         	.withArgs(voter2.address);
 		});
 
-
 		
-
-		
-		it("Should revert with voter Voters registration is not open yet", async () => {
-			await voting.startProposalsRegistering();
-			await expect(voting.addVoter(voter1.address)).to.be.revertedWith('Voters registration is not open yet');
-		});
-
-		it("Should revert with voter Already registered", async () => {
+		// Voters checks
+		it("Should find registered voter", async () => {
 			await voting.addVoter(voter1.address);
-			await expect(voting.addVoter(voter1.address)).to.be.revertedWith('Already registered');
+			const voter = await voting.connect(voter1).getVoter(voter1.address);
+			await expect(voter.isRegistered).to.equal(true);
 		});
 
 		it("Should emit event VoterRegistered", async () => {
@@ -108,8 +105,19 @@ describe("Voting", () => {
 			.to.emit(voting, "VoterRegistered")
 			.withArgs(voter1.address);
 		});
-
 		
+		it("Should revert with voter Already registered", async () => {
+			await voting.addVoter(voter1.address);
+			await expect(voting.addVoter(voter1.address)).to.be.revertedWith('Already registered');
+		});
+
+		it("Should revert with voter Voters registration is not open yet", async () => {
+			await voting.startProposalsRegistering();
+			await expect(voting.addVoter(voter1.address)).to.be.revertedWith('Voters registration is not open yet');
+		});
+
+
+		// Proposals checks
 
 		it("Should revert with Proposals are not allowed yet", async () => {
 			await voting.addVoter(voter1.address);
@@ -122,16 +130,25 @@ describe("Voting", () => {
 			await expect(voting.connect(voter1).addProposal("")).to.be.revertedWith('Vous ne pouvez pas ne rien proposer');
 		});
 
-		it("Should find registered voter", async () => {
-			await voting.addVoter(voter1.address);
-			const voter = await voting.connect(voter1).getVoter(voter1.address);
-			await expect(voter.isRegistered).to.equal(true);
-		});
-
 		it("Should emit event WorkflowStatusChange after startProposalsRegistering", async () => {
 		    await expect(voting.startProposalsRegistering())
 			.to.emit(voting, "WorkflowStatusChange")
 			.withArgs(0,1);
+		});
+		
+		it("Should retrieve added proposal", async () => {
+			await voting.addVoter(voter1.address);
+			await voting.startProposalsRegistering();
+			await voting.connect(voter1).addProposal("test");
+			const proposal = await voting.connect(voter1).getOneProposal(1);
+			await expect(proposal.description).to.equal("test");
+		});
+
+		it("Should emit event ProposalRegistered", async () => {
+			await voting.addVoter(voter1.address);
+			await voting.startProposalsRegistering();
+		    await expect(voting.connect(voter1).addProposal("test"))
+			.to.emit(voting, "ProposalRegistered");
 		});
 
 		it("Should emit event WorkflowStatusChange after endProposalsRegistering", async () => {
@@ -140,6 +157,9 @@ describe("Voting", () => {
 			.to.emit(voting, "WorkflowStatusChange")
 			.withArgs(1,2);
 		});
+
+
+		// Voting checks
 
 		it("Should emit event WorkflowStatusChange after startVotingSession", async () => {
 		    await voting.startProposalsRegistering();
@@ -158,32 +178,6 @@ describe("Voting", () => {
 			.withArgs(3,4);
 		});
 
-		it("Should emit event WorkflowStatusChange after tallyVotes", async () => {
-		    await voting.startProposalsRegistering();
-		    await voting.endProposalsRegistering();
-		    await voting.startVotingSession();
-		    await voting.endVotingSession();
-		    await expect(voting.tallyVotes())
-			.to.emit(voting, "WorkflowStatusChange")
-			.withArgs(4,5);
-		});
-
-		it("Should retrieve added proposal", async () => {
-			await voting.addVoter(voter1.address);
-			await voting.startProposalsRegistering();
-			await voting.connect(voter1).addProposal("test");
-			const proposal = await voting.connect(voter1).getOneProposal(1);
-			await expect(proposal.description).to.equal("test");
-		});
-
-		it("Should emit event ProposalRegistered", async () => {
-			await voting.addVoter(voter1.address);
-			await voting.startProposalsRegistering();
-		    await expect(voting.connect(voter1).addProposal("test"))
-			.to.emit(voting, "ProposalRegistered");
-		});
-
-
 		it("Should retrieve added vote to proposal and emit event Voted", async () => {
 			await voting.addVoter(voter1.address);
 			await voting.addVoter(voter2.address);
@@ -196,6 +190,36 @@ describe("Voting", () => {
 			.withArgs(voter2.address,1);
 			const proposal = await voting.connect(voter1).getOneProposal(1);
 			await expect(proposal.voteCount).to.equal(1);
+		});
+
+
+		// Tally votes checks
+
+		it("winningProposalID should be equal to 2 after tallyVotes", async () => {
+			await voting.addVoter(voter1.address);
+			await voting.addVoter(voter2.address);
+			await voting.addVoter(voter3.address);
+			await voting.startProposalsRegistering();
+			await voting.connect(voter1).addProposal("Proposal1");
+			await voting.connect(voter2).addProposal("Proposal2");
+		    await voting.endProposalsRegistering();
+		    await voting.startVotingSession();
+		    await voting.connect(voter1).setVote(1);
+		    await voting.connect(voter2).setVote(2);
+		    await voting.connect(voter3).setVote(2);
+		    await voting.endVotingSession();
+		    await voting.tallyVotes();
+			expect(await voting.winningProposalID()).to.equal(2);
+		});
+		
+		it("Should emit event WorkflowStatusChange after tallyVotes", async () => {
+		    await voting.startProposalsRegistering();
+		    await voting.endProposalsRegistering();
+		    await voting.startVotingSession();
+		    await voting.endVotingSession();
+		    await expect(voting.tallyVotes())
+			.to.emit(voting, "WorkflowStatusChange")
+			.withArgs(4,5);
 		});
 
 	});
